70bc9b898f4910364210f2dc846000ac4e306fe8
huntingforheat <asktt1234@gmail.com>
2024-07-01 16:40:21 +0900

    Initial commit

---
+++ src/main/java/com/daewon/xeno_z1/XenoZ1Application.java
@@ -0,0 +1,13 @@
+package com.daewon.xeno_z1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XenoZ1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XenoZ1Application.class, args);
+	}
+
+}
