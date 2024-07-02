0745544e70adda63b09c900f6b9acc09355ba049
huntingforheat <asktt1234@gmail.com>
2024-07-02 12:56:01 +0900

    ..

--- XENO_backend/src/main/java/com/daewon/xeno_z1/repository/ReviewImageRepository.java
+++ XENO_backend/src/main/java/com/daewon/xeno_z1/repository/ReviewImageRepository.java
@@ -2,11 +2,16 @@ package com.daewon.xeno_z1.repository;
 
 
 import com.daewon.xeno_z1.domain.ReviewImage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+
 @Repository
-public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long>{
+public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
 
     // 제품 상세 페이지 이미지 총 갯수
     @Query("SELECT COUNT(rie) FROM ReviewImageEntity rie WHERE rie.review.products.productId = :productId")
@@ -14,6 +19,6 @@ public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long>{
 
     // 제품의 전체 후기 사진
     @Query("SELECT rie FROM ReviewImageEntity rie WHERE rie.review.products.productId = :productId")
-    Page<ReviewImageEntity> findReviewImagesByProductId(@Param("productId") Long productId, Pageable pageable);
+    Page<ReviewImage> findReviewImagesByProductId(@Param("productId") Long productId, Pageable pageable);
 
 }
