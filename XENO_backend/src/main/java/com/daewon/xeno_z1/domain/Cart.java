643104e56b6a1b00d24fcdbca918c9a2a7c42b67
huntingforheat <asktt1234@gmail.com>
2024-07-04 09:38:57 +0900

    cart entity

--- XENO_backend/src/main/java/com/daewon/xeno_z1/domain/Cart.java
+++ XENO_backend/src/main/java/com/daewon/xeno_z1/domain/Cart.java
@@ -3,6 +3,8 @@ package com.daewon.xeno_z1.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Getter
@@ -19,9 +21,19 @@ public class Cart {
     @JoinColumn(name = "user_id", nullable = false)
     private Users user;
 
-    @ManyToOne
-    @JoinColumn(name = "product_id", nullable = false)
-    private Products product;
+//    @ManyToOne
+//    @JoinColumn(name = "product_id", nullable = false)
+//    private Products product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_color_size", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private ProductsColorSize productsColorSize;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_image_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private ProductsImage productsImage;
 
     @Column(name = "quantity", nullable = false)
     private Long quantity;  // 수량
@@ -29,9 +41,10 @@ public class Cart {
     @Column(name = "price", nullable = false)
     private Long price;
 
-    public Cart(Users user, Products product, Long quantity, Long price) {
+    public Cart(Users user, ProductsColorSize productsColorSize, ProductsImage productsImage, Long quantity, Long price) {
         this.user = user;
-        this.product = product;
+        this.productsColorSize = productsColorSize;
+        this.productsImage = productsImage;
         this.quantity = quantity;
         this.price = price;
     }
