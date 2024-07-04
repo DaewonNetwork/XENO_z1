878ddb5a2361ca348d2a0ff2e871800fda864849
huntingforheat <asktt1234@gmail.com>
2024-07-04 16:36:55 +0900

    cart...ing

--- client/src/app/(app)/cart/page.tsx
+++ client/src/app/(app)/cart/page.tsx
@@ -1,10 +1,13 @@
+import AppSection from "@/(FSD)/widgets/app/ui/AppSection";
+import CartProductList from "@/(FSD)/widgets/cart/ui/CartProductList";
 import React from "react";
 
 const Page = () => {
     return (
-        <div>
-
-        </div>
+        <AppSection>
+        
+        <CartProductList />
+        </AppSection>
     );
 };
 
