b046ad52c730a305ff003816322c7b2a74bcc035
huntingforheat <asktt1234@gmail.com>
2024-07-08 16:59:35 +0900

    프론트 하는중.

--- client/src/(FSD)/shareds/fetch/fetchData.ts
+++ client/src/(FSD)/shareds/fetch/fetchData.ts
@@ -1,44 +1,90 @@
+// "use client"
+
+// import { useRecoilValue } from "recoil";
+// import { FetchType } from "../types/FetchData.type";
+// import { userState } from "../stores/UserAtom";
+
+// export const    fetchData = async ({ path, method = "GET", contentType = "application/json", isAuthRequired = false, isNotAuthRequired = false, body }: FetchType) => {
+//     let response = null;
+
+//     const { isLoggedIn, accessToken } = useRecoilValue(userState);
+
+//     console.log(isLoggedIn);
+    
+//     if ((!isNotAuthRequired && isLoggedIn) || (isAuthRequired && isLoggedIn)) {
+//         response = await fetch(`http://localhost:8090/api${path}`, {
+//             method: method,
+//             headers: {
+//                 "Content-Type": contentType,
+//                 "Authorization": `Bearer ${accessToken}`,
+//             },
+//             body: JSON.stringify(body)
+//         });
+//     } else {
+//         response = await fetch(`http://localhost:8090${path}`, {
+//             method: method,
+//             headers: {
+//                 "Content-Type": contentType,
+//             },
+//             body: JSON.stringify(body)
+//         });
+//     };
+
+//     if (!response.ok) {
+//         const errorMessage = await response.text();
+//         throw new Error(errorMessage);
+//     };
+
+//     console.log(response);
+    
+
+//     const data = await response.json();
+
+//     return data;
+// };
+
 "use client"
 
 import { useRecoilValue } from "recoil";
 import { FetchType } from "../types/FetchData.type";
 import { userState } from "../stores/UserAtom";
 
-export const    fetchData = async ({ path, method = "GET", contentType = "application/json", isAuthRequired = false, isNotAuthRequired = false, body }: FetchType) => {
+export const useFetchData = () => {
+  const { isLoggedIn, accessToken } = useRecoilValue(userState);
+
+  return async ({ path, method = "GET", contentType = "application/json", isAuthRequired = false, isNotAuthRequired = false, body }: FetchType) => {
     let response = null;
 
-    const { isLoggedIn, accessToken } = useRecoilValue(userState);
-
-    console.log(isLoggedIn);
+    console.log("isLoggedIn:", isLoggedIn);
     
     if ((!isNotAuthRequired && isLoggedIn) || (isAuthRequired && isLoggedIn)) {
-        response = await fetch(`http://localhost:8090/api${path}`, {
-            method: method,
-            headers: {
-                "Content-Type": contentType,
-                "Authorization": `Bearer ${accessToken}`,
-            },
-            body: JSON.stringify(body)
-        });
+      response = await fetch(`http://localhost:8090/api${path}`, {
+        method: method,
+        headers: {
+          "Content-Type": contentType,
+          "Authorization": `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify(body)
+      });
     } else {
-        response = await fetch(`http://localhost:8090${path}`, {
-            method: method,
-            headers: {
-                "Content-Type": contentType,
-            },
-            body: JSON.stringify(body)
-        });
-    };
+      response = await fetch(`http://localhost:8090${path}`, {
+        method: method,
+        headers: {
+          "Content-Type": contentType,
+        },
+        body: JSON.stringify(body)
+      });
+    }
 
     if (!response.ok) {
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
-    };
+      const errorMessage = await response.text();
+      throw new Error(errorMessage);
+    }
 
     console.log(response);
-    
 
     const data = await response.json();
 
     return data;
+  };
 };
