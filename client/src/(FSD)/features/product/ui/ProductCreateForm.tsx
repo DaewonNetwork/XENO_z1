745d46efea5c3dab5e8c286e984f3b9a5a88301a
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2024-07-09 17:37:30 +0900

    ㅁ

--- client/src/(FSD)/features/product/ui/ProductCreateForm.tsx
+++ client/src/(FSD)/features/product/ui/ProductCreateForm.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import styles from "@/(FSD)/shareds/styles/ProductStyle.module.scss";
+import FormInputShared from "@/(FSD)/shareds/ui/FormInputShared";
 
 const ProductCreateForm = () => {
+    const [category, setCategory] = useState();
+    const [subCategory, setSubCategory] = useState();
+
     const schema = z.object({
+        name: z.string(),
+        price: z.number(),
+        price_sale: z.number(),
+        product_number: z.string(),
+        season: z.string(),
     });
 
     const { control, handleSubmit, formState: { errors, isValid, submitCount } } = useForm({
@@ -21,6 +30,11 @@ const ProductCreateForm = () => {
 
     return (
         <form className={styles.product_create_form} onSubmit={handleSubmit(onSubmit)}>
+            <FormInputShared isClearable autoFocus={true} size={"lg"} variant={"flat"} isInvalid={!!errors.name} radius={"none"} errorMessage={errors.name && <>{errors.name.message}</>} name={"name"} control={control} placeholder={"상품명을 입력해주세요."} />
+            <FormInputShared isClearable size={"lg"} variant={"flat"} isInvalid={!!errors.product_number} radius={"none"} errorMessage={errors.product_number && <>{errors.product_number.message}</>} name={"product_number"} control={control} placeholder={"품번을 입력해주세요."} />
+            <FormInputShared isClearable size={"lg"} variant={"flat"} isInvalid={!!errors.season} radius={"none"} errorMessage={errors.season && <>{errors.season.message}</>} name={"season"} control={control} placeholder={"시즌을 입력해주세요."} />
+            
+            <FormInputShared isClearable size={"lg"} variant={"flat"} isInvalid={!!errors.price} radius={"none"} errorMessage={errors.price && <>{errors.price.message}</>} name={"price"} control={control} placeholder={"가격을 입력해주세요."} />
 
         </form>
     );
