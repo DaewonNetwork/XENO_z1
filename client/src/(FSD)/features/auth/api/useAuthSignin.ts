8948d6cfac17f3a892a5c5210c68f382e5fe853e
huntingforheat <asktt1234@gmail.com>
2024-07-09 10:55:01 +0900

    fetchdata finish

--- client/src/(FSD)/features/auth/api/useAuthSignin.ts
+++ client/src/(FSD)/features/auth/api/useAuthSignin.ts
@@ -1,9 +1,12 @@
 import { useMutation } from "@tanstack/react-query";
 import { MutationType } from "../../types/mutation.type";
 import { UserType } from "@/(FSD)/shareds/types/User.type";
-import { fetchData } from "@/(FSD)/shareds/fetch/fetchData";
+import useFetchData from "@/(FSD)/shareds/fetch/useFetchData";
+
 
 export const useAuthSignin = ({ onSuccess, onError }: MutationType) => {
+    const fetchData = useFetchData();
+    
     return useMutation({
         mutationFn: (userData: UserType) => {
             return fetchData({ path: "/auth/signin", method: "POST", body: userData, isNotAuthRequired: true })
