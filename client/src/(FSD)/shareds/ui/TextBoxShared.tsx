e24fd10a84a8ba818580d525066058410a35eaa7
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2024-07-10 10:35:13 +0900

    save

--- client/src/(FSD)/shareds/ui/TextBoxShared.tsx
+++ client/src/(FSD)/shareds/ui/TextBoxShared.tsx
@@ -6,9 +6,9 @@ interface TextBoxType extends TextType {
     isBgColor?: boolean;
 };
 
-const TextBoxShared = ({ children, className = "", isBgColor = false }: TextBoxType) => {
+const TextBoxShared = ({ children, className = "", fontWeight = "medium", isBgColor = false }: TextBoxType) => {
     return (
-        <div data-slot={"text_box"} className={`${className} ${isBgColor ? "bg-content3 rounded-small" : ""} ${styles.text_box}`}>
+        <div data-slot={"text_box"} className={`${className} font-${fontWeight} ${isBgColor ? "bg-content3 rounded-small" : ""} ${styles.text_box}`}>
             {children}
         </div>
     )
