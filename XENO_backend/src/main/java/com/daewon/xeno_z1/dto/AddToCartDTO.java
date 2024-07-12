745c193a764d4c588890e683ed4b80b82bffb015
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-07-12 15:17:56 +0900

    asd

---
+++ XENO_backend/src/main/java/com/daewon/xeno_z1/dto/AddToCartDTO.java
@@ -0,0 +1,21 @@
+package com.daewon.xeno_z1.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AddToCartDTO {
+
+    private Long productColorSizeId;
+
+    private Long quantity;  // 수량
+
+    private Long price;
+
+}
